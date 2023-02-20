8eec73f407cb545647c3e6114c7dd564e96a829d
Lenny Bontenakel <lenny@Lennys-MacBook-Pro.local>
2023-02-20 13:10:40 +0100

    testing results

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,4 +1,14 @@
 (in-package :mu-cl-resources)
 
-;; reading in the domain.json
-(read-domain-file "domain.json")
+(defparameter *cache-count-queries* nil)
+(defparameter *supply-cache-headers-p* t
+  "when non-nil, cache headers are supplied.  this works together with mu-cache.")
+(setf *cache-model-properties-p* t)
+
+(defparameter *include-count-in-paginated-responses* t
+  "when non-nil, all paginated listings will contain the number
+   of responses in the result object's meta.")
+(defparameter *max-group-sorted-properties* nil)
+
+(read-domain-file "domain_taxtypes.json")
+
