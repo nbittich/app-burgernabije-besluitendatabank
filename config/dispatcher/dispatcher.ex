a7403cddf7e69a55a571546bea4bef10c37d423a
Lenny Bontenakel <lenny@Lennys-MacBook-Pro.local>
2023-03-02 19:52:33 +0100

    added uuid generator

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -23,6 +23,11 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://resources/"
   end
 
+  get "/uuid-generator/*path", @json do
+    Proxy.forward conn, path, "http://uuid-generator/"
+  end
+
+
   match "/*_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
