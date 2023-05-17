425232412fa4dead5c3fab2a0381448d28002ef4
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-05-17 18:19:36 +0200

    Refactor: Use lblod shared domain resources configuration

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -1,69 +1,65 @@
-  defmodule Dispatcher do
-    use Matcher
-    define_accept_types [
-      html: [ "text/html", "application/xhtml+html" ],
-      json: [ "application/json", "application/vnd.api+json" ]
-    ]
+defmodule Dispatcher do
+  use Matcher
+  define_accept_types [
+    html: [ "text/html", "application/xhtml+html" ],
+    json: [ "application/json", "application/vnd.api+json" ]
+  ]
 
-    @any %{}
-    @json %{ accept: %{ json: true } }
-    @html %{ accept: %{ html: true } }
+  @any %{}
+  @json %{ accept: %{ json: true } }
+  @html %{ accept: %{ html: true } }
 
-    # In order to forward the 'themes' resource to the
-    # resource service, use the following forward rule:
-    #
-    # match "/themes/*path", @json do
-    #   Proxy.forward conn, path, "http://resource/themes/"
-    # end
-    #
-    # Run `docker-compose restart dispatcher` after updating
-    # this file.
+  # In order to forward the 'themes' resource to the
+  # resource service, use the following forward rule:
+  #
+  # match "/themes/*path", @json do
+  #   Proxy.forward conn, path, "http://resource/themes/"
+  # end
+  #
+  # Run `docker-compose restart dispatcher` after updating
+  # this file.
 
-  # RESOURCES 
-
-    get "/decisions/*path", @json do
-      Proxy.forward conn, path, "http://resources/decisions"
-    end
-
-    get "/agenda-items/*path", @json do
-      Proxy.forward conn, path, "http://resources/agenda-items"
-    end
-
-    get "/agenda-item-treatments/*path", @json do
-      Proxy.forward conn, path, "http://resources/agenda-item-treatments"
-    end
-
-    get "/sessions/*path", @json do
-      Proxy.forward conn, path, "http://resources/sessions"
-    end
-    
-    get "/governing-agents/*path", @json do
-      Proxy.forward conn, path, "http://resources/governing-agents"
-    end
-    
-    get "/governing-units/*path", @json do
-      Proxy.forward conn, path, "http://resources/governing-units"
-    end
-    
-    get "/agents/*path", @json do
-      Proxy.forward conn, path, "http://resources/agents"
-    end
-
-    get "/votings/*path", @json do
-      Proxy.forward conn, path, "http://resources/votings"
-    end
-
-    get "/locations/*path", @json do
-      Proxy.forward conn, path, "http://resources/locations"
-    end
-
-  # SERVICES
-
-    get "/*path", @json do
-      Proxy.forward conn, path, "http://frontend/"
-    end
-
-    match "/*_", %{ last_call: true } do
-      send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
-    end
+  ###############
+  # RESOURCES
+  ###############
+  get "/agendapunten/*path", @json do
+    Proxy.forward conn, path, "http://resources/agendapunten/"
   end
+
+  get "/behandelingen-van-agendapunten/*path", @json do
+    Proxy.forward conn, path, "http://resources/behandelingen-van-agendapunten/"
+  end
+
+  get "/stemmingen/*path", @json do
+    Proxy.forward conn, path, "http://resources/stemmingen/"
+  end
+
+  get "/zittingen/*path", @json do
+    Proxy.forward conn, path, "http://resources/zittingen/"
+  end
+
+  get "/mandatarissen/*path", @json do
+    Proxy.forward conn, path, "http://resources/mandatarissen/"
+  end
+
+  ###############
+  # SERVICES
+  ###############
+  match "/uuid-generator/*path", @json do
+    Proxy.forward conn, path, "http://uuid-generator/"
+  end
+
+  ###############
+  # FRONTEND
+  ###############
+  get "/*path", @json do
+    Proxy.forward conn, path, "http://frontend/"
+  end
+
+  #################
+  # NOT FOUND
+  #################
+  match "/*_", %{ last_call: true } do
+    send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
+  end
+end
--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,23 +1,4 @@
 (in-package :mu-cl-resources)
 
-(defparameter *cache-count-queries* nil)
-(defparameter *supply-cache-headers-p* t
-  "when non-nil, cache headers are supplied.  this works together with mu-cache.")
-(setf *cache-model-properties-p* t)
-
-(defparameter *include-count-in-paginated-responses* t
-  "when non-nil, all paginated listings will contain the number
-   of responses in the result object's meta.")
-(defparameter *max-group-sorted-properties* nil)
-
-(read-domain-file "domain_agenda-items.json")
-(read-domain-file "domain_decisions.json")
-
-(read-domain-file "subdomain_sessions.json")
-(read-domain-file "subdomain_agenda-item-treatments.json")
-(read-domain-file "subdomain_votings.json")
-(read-domain-file "subdomain_agents.json")
-
-(read-domain-file "subdomain_governing-agents.json")
-(read-domain-file "subdomain_governing-units.json")
-(read-domain-file "subdomain_locations.json")
+(read-domain-file "slave-besluit-domain.lisp")
+(read-domain-file "slave-mandaat-domain.lisp")
