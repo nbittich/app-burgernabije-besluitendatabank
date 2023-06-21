312ea9224610d6411031eb121b85ce6b113d9e4e
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-06-21 12:43:05 +0200

    Feat(cache): Add cache clearing with delta-notifier
    Wire delta-notifier, mu-cl-resources and mu-cache

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -25,5 +25,19 @@ export default [
             gracePeriod: 250,
             ignoreFromSelf: false
         }
-    }))
+    })),
+    {
+        match: {
+          subject: { }
+        },
+        callback: {
+          url: "http://resources/.mu/delta",
+          method: "POST"
+        },
+        options: {
+          resourceFormat: "v0.0.1",
+          gracePeriod: 250,
+          ignoreFromSelf: true
+        }
+      }
 ]
