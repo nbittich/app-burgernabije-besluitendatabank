07648fcfe22d077cd056883649309a4e4703884d
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-07-06 09:53:17 +0200

    Feat(Resources): Add count in response

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -3,6 +3,8 @@
 ;; https://github.com/mu-semtech/mu-cl-resources/blob/master/README.md#external-cache
 (defparameter *cache-model-properties* t)
 (defparameter *supply-cache-headers-p* t)
+;; add the total amount of result in the response (meta.count)
+(defparameter *include-count-in-paginated-responses* t)
 
 (read-domain-file "besluit-domain-en.lisp")
 (read-domain-file "mandaat-domain-en.lisp")
