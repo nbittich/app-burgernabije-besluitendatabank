3f8878ca52f88c599c24cd60c7174cf923eab2e5
felix <ruiz.felixruiz@gmail.com>
2023-10-30 17:08:13 +0100

    update reset elasetic script

--- scripts/reset-elastic.sh
+++ scripts/reset-elastic.sh
@@ -3,12 +3,12 @@
 set -e
 echo "warning this will run queries on the triplestore and delete containers, you have 3 seconds to press ctrl+c"
 sleep 3
-docker-compose rm -fs elasticsearch search
+docker compose rm -fs elasticsearch search
 sudo rm -rf data/elasticsearch/
-docker-compose up -d triplestore
-docker-compose exec -T triplestore isql-v <<EOF
+docker compose up -d triplestore
+docker compose exec -T triplestore isql-v <<EOF
 SPARQL DELETE WHERE {   GRAPH <http://mu.semte.ch/authorization> {     ?s ?p ?o.   } };
 exec('checkpoint');
 exit;
 EOF
-docker-compose up -d --remove-orphan
+docker compose up -d --remove-orphans
