531fabc220ddd73ab680321f7980aa86f41f9359
Nordine Bittich <nordine@bittich.be>
2023-11-09 13:39:19 +0100

    parallel calls

---
+++ config/consumer/besluiten/delta-sync-dispatching.js
@@ -0,0 +1,80 @@
+const {
+  BYPASS_MU_AUTH_FOR_EXPENSIVE_QUERIES,
+  DIRECT_DATABASE_ENDPOINT,
+  PARALLEL_CALLS,
+  BATCH_SIZE,
+  SLEEP_BETWEEN_BATCHES,
+  INGEST_GRAPH,
+} = require("./config.js");
+const { batchedUpdate } = require("./utils");
+const endpoint = BYPASS_MU_AUTH_FOR_EXPENSIVE_QUERIES
+  ? DIRECT_DATABASE_ENDPOINT
+  : process.env.MU_SPARQL_ENDPOINT; //Defaults to mu-auth
+
+/**
+ * Dispatch the fetched information to a target graph.
+ * @param { mu, muAuthSudo, fetch } lib - The provided libraries from the host service.
+ * @param { termObjectChangeSets: { deletes, inserts } } data - The fetched changes sets, which objects of serialized Terms
+ *          [ {
+ *              graph: "<http://foo>",
+ *              subject: "<http://bar>",
+ *              predicate: "<http://baz>",
+ *              object: "<http://boom>^^<http://datatype>"
+ *            }
+ *         ]
+ * @return {void} Nothing
+ */
+async function dispatch(lib, data) {
+  const { mu, chunk } = lib;
+  const { termObjectChangeSets } = data;
+
+  const chunks = chunk(termObjectChangeSets, PARALLEL_CALLS);
+
+  for (const chunkedArray of chunks) {
+    await Promise.all(
+      chunkedArray.map(async (delta) => {
+        const { deletes, inserts } = delta;
+
+        if (BYPASS_MU_AUTH_FOR_EXPENSIVE_QUERIES) {
+          console.warn(`Service configured to skip MU_AUTH!`);
+        }
+
+        console.log(`Using ${endpoint} to insert triples`);
+
+        const deleteStatements = deletes.map(
+          (o) => `${o.subject} ${o.predicate} ${o.object}.`,
+        );
+
+        await batchedUpdate(
+          lib,
+          deleteStatements,
+          INGEST_GRAPH,
+          SLEEP_BETWEEN_BATCHES,
+          BATCH_SIZE,
+          {},
+          endpoint,
+          "DELETE",
+        );
+
+        const insertStatements = inserts.map(
+          (o) => `${o.subject} ${o.predicate} ${o.object}.`,
+        );
+
+        await batchedUpdate(
+          lib,
+          insertStatements,
+          INGEST_GRAPH,
+          SLEEP_BETWEEN_BATCHES,
+          BATCH_SIZE,
+          {},
+          endpoint,
+          "INSERT",
+        );
+      }),
+    );
+  }
+}
+
+module.exports = {
+  dispatch,
+};
