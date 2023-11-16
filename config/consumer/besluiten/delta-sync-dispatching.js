ad80f96d1c2aaa8d07cfdfae757f86d47e113280
Nordine Bittich <nordine@bittich.be>
2023-11-16 14:14:50 +0100

    give a second chance with direct call to virtuoso

--- config/consumer/besluiten/delta-sync-dispatching.js
+++ config/consumer/besluiten/delta-sync-dispatching.js
@@ -12,6 +12,50 @@ const endpoint = BYPASS_MU_AUTH_FOR_EXPENSIVE_QUERIES
   : process.env.MU_SPARQL_ENDPOINT; //Defaults to mu-auth
 
 /**
+ * Ingest delta function. return true if it succeeds, false otherwise
+ *
+ */
+async function ingest(lib, delta, triplestoreEndpoint) {
+  try {
+    console.log(`Using ${triplestoreEndpoint} to insert triples`);
+    const { deletes, inserts } = delta;
+    const deleteStatements = deletes.map(
+      (o) => `${o.subject} ${o.predicate} ${o.object}.`,
+    );
+    await batchedUpdate(
+      lib,
+      deleteStatements,
+      INGEST_GRAPH,
+      SLEEP_BETWEEN_BATCHES,
+      BATCH_SIZE,
+      {},
+      triplestoreEndpoint,
+      "DELETE",
+    );
+    const insertStatements = inserts.map(
+      (o) => `${o.subject} ${o.predicate} ${o.object}.`,
+    );
+
+    await batchedUpdate(
+      lib,
+      insertStatements,
+      INGEST_GRAPH,
+      SLEEP_BETWEEN_BATCHES,
+      BATCH_SIZE,
+      {},
+      triplestoreEndpoint,
+      "INSERT",
+    );
+    return true;
+  } catch (e) {
+    console.log(
+      `Failed to ingest delta message: ${e}, delta: ${JSON.stringify(delta)}`,
+    );
+    return false;
+  }
+}
+
+/**
  * Dispatch the fetched information to a target graph.
  * @param { mu, muAuthSudo, fetch } lib - The provided libraries from the host service.
  * @param { termObjectChangeSets: { deletes, inserts } } data - The fetched changes sets, which objects of serialized Terms
@@ -33,43 +77,18 @@ async function dispatch(lib, data) {
   for (const chunkedArray of chunks) {
     await Promise.all(
       chunkedArray.map(async (delta) => {
-        const { deletes, inserts } = delta;
-
         if (BYPASS_MU_AUTH_FOR_EXPENSIVE_QUERIES) {
           console.warn(`Service configured to skip MU_AUTH!`);
         }
-
-        console.log(`Using ${endpoint} to insert triples`);
-
-        const deleteStatements = deletes.map(
-          (o) => `${o.subject} ${o.predicate} ${o.object}.`,
-        );
-
-        await batchedUpdate(
-          lib,
-          deleteStatements,
-          INGEST_GRAPH,
-          SLEEP_BETWEEN_BATCHES,
-          BATCH_SIZE,
-          {},
-          endpoint,
-          "DELETE",
-        );
-
-        const insertStatements = inserts.map(
-          (o) => `${o.subject} ${o.predicate} ${o.object}.`,
-        );
-
-        await batchedUpdate(
-          lib,
-          insertStatements,
-          INGEST_GRAPH,
-          SLEEP_BETWEEN_BATCHES,
-          BATCH_SIZE,
-          {},
-          endpoint,
-          "INSERT",
-        );
+        let succeeded = await ingest(lib, delta, endpoint);
+        if (
+          !succeeded &&
+          !BYPASS_MU_AUTH_FOR_EXPENSIVE_QUERIES &&
+          DIRECT_DATABASE_ENDPOINT
+        ) {
+          console.log("attempt with a direct call to virtuoso this time...");
+          await ingest(lib, delta, DIRECT_DATABASE_ENDPOINT);
+        }
       }),
     );
   }
